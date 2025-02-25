fdd347a86ba3eff9c83fec6c8f8b2dbc87d14994
Foster <mbayifoster@gmail.com>
2025-02-25 14:08:47 +0100

    utilisation de obx

--- lib/views/pages/acceuil/home_page.dart
+++ lib/views/pages/acceuil/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jenos_app/controllers/home/controllers/home_ctrl.dart';
+import 'package:jenos_app/models/principals/plat.dart';
 import 'package:jenos_app/utils/colors.dart';
 import 'package:jenos_app/views/components/buttons/panier_button.dart';
+import 'package:jenos_app/views/components/chargement.dart';
 import 'package:jenos_app/views/components/inputs/input_search.dart';
 import 'package:jenos_app/views/components/my_bottom_navigation_bar.dart';
 import 'package:jenos_app/views/components/buttons/my_floating_button.dart';
@@ -21,43 +23,63 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   HomeCtrl ctrl = Get.put(HomeCtrl());
+
   List plats = [1, 2, 3, 4, 5, 6, 7];
 
+  /*  @override
+  void initState() {
+    super.initState();
+    ctrl.fetchData();
+  }
+ */
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    return GetBuilder<HomeCtrl>(builder: (ctrl) {
-      return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: TextTitle(title: "Bonjour Rolly !"),
-          automaticallyImplyLeading: false,
-          // ignore: prefer_const_literals_to_create_immutables
-          actions: [const PanierButton()],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                    bottom: 45.00, top: 25, left: 25.00, right: 25.00),
-                child: InputSearch(
-                  tap: () {},
-                ),
-              ),
-              _offres(),
-              _platPop(width),
-              _platMostPop(width),
-              _recents(width)
-            ],
-          ),
-        ),
-        bottomNavigationBar: const MyBottomNavigationBar(),
-        floatingActionButton: const MyFloatingButton(),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      );
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Mettez à jour l'état ici
+      ctrl.fetchData();
     });
+
+    var state = ctrl.state;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: TextTitle(title: "Bonjour Rolly !"),
+        automaticallyImplyLeading: false,
+        // ignore: prefer_const_literals_to_create_immutables
+        actions: [const PanierButton()],
+      ),
+      body: Obx(() {
+        if (state.value.visible) {
+          List<Plat> recents = state.value.platRecents;
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      bottom: 45.00, top: 25, left: 25.00, right: 25.00),
+                  child: InputSearch(
+                    tap: () {},
+                  ),
+                ),
+                _offres(),
+                _platPop(width),
+                _platMostPop(width),
+                _recents(width, plats: recents)
+              ],
+            ),
+          );
+        }
+        return Chargement(
+          loading: state.value.loading,
+          hasData: state.value.hasData,
+        );
+      }),
+      bottomNavigationBar: const MyBottomNavigationBar(),
+      floatingActionButton: const MyFloatingButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 
   _offres() {
@@ -167,7 +189,8 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _recents(width) {
+  // ignore: unused_element
+  _recents(width, {List<Plat> plats =const []}) {
     return Column(
       children: [
         Padding(
@@ -193,11 +216,11 @@ class _HomePageState extends State<HomePage> {
         const SizedBox(
           height: 16.00,
         ),
-        for (var i = 0; i < 3; i++)
+        for (Plat plat in plats)
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.00),
             child: Column(
-              children: [PlatRecent(tap: () {}), const SizedBox(height: 25.00)],
+              children: [PlatRecent(tap: () {}, plat: plat,), const SizedBox(height: 25.00)],
             ),
           ),
       ],
