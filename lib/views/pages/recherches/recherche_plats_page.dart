a7341e4d1afa7a1545912e2d6190fe54500454ed
louis370 <kalalanzajilouisdor@gmail.com>
2025-03-18 01:26:23 +0100

    ajout pâge recherche_plats_page

---
+++ lib/views/pages/recherches/recherche_plats_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecherchePlatsPage extends StatefulWidget {
+  const RecherchePlatsPage({super.key});
+
+  @override
+  State<RecherchePlatsPage> createState() => _RecherchePlatsPageState();
+}
+
+class _RecherchePlatsPageState extends State<RecherchePlatsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
