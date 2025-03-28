a608534504a0b4563a6982e57909ea34cdbb75f4
Foster <mbayifoster@gmail.com>
2025-03-28 16:24:36 +0100

    ajout de la possibilité de refreshir la page

--- lib/views/pages/commandes/panier_achat/panier_achat.dart
+++ lib/views/pages/commandes/panier_achat/panier_achat.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jenos_app/models/principals/panier.dart';
+import 'package:jenos_app/utils/colors.dart';
 import 'package:jenos_app/views/components/buttons/primary_button.dart';
 import 'package:jenos_app/views/pages/commandes/panier_achat/panier_achat_ctrl.dart';
 import 'package:jenos_app/views/components/cards/plat_panier.dart';
@@ -34,94 +35,103 @@ class _PanierAchatState extends State<PanierAchat> {
                     ctrl.allCheck();
                   },
                   child: TextTitle(
-                    fontSize: 16,
+                      fontSize: 16,
                       title: (state.value.paniers.length ==
                               state.value.checkList.length)
                           ? "Tous deselectionner (${state.value.checkList.length})"
                           : "Tout selectionner (${state.value.checkList.length})"))
             ],
           ),
-          body: (state.value.visible)
-              ? Stack(children: [
-                  SingleChildScrollView(
-                    child: Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        children: [
-                          for (Panier panier in state.value.paniers)
-                            Column(
-                              children: [
-                                PlatPanier(
-                                  isChecked: (state.value.checkList
-                                          .contains(panier.id))
-                                      ? true
-                                      : false,
-                                  panier: panier,
-                                  tap: () {
-                                    ctrl.check(panier);
-                                  },
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                              ],
-                            ),
-                        ],
+          body: RefreshIndicator(
+            child: (state.value.visible)
+                ? Stack(children: [
+                    SingleChildScrollView(
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            for (Panier panier in state.value.paniers)
+                              Column(
+                                children: [
+                                  PlatPanier(
+                                    isChecked: (state.value.checkList
+                                            .contains(panier.id))
+                                        ? true
+                                        : false,
+                                    panier: panier,
+                                    tap: () {
+                                      ctrl.check(panier);
+                                    },
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                ],
+                              ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white, // Couleur de fond
-                          /*  border: Border(
+                    Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white, // Couleur de fond
+                            /*  border: Border(
                      op: BorderSide(
                         color: Colors.black, // Couleur de la bordure
                         width: 0.5, // Épaisseur de la bordure
                       ),
                     ), */
-                          boxShadow: [
-                            BoxShadow(
-                              color:
-                                  // ignore: deprecated_member_use
-                                  Colors.black45, // Couleur de l'ombre
-                              spreadRadius: 2, // Rayon de propagation
-                              blurRadius: 5, // Flou de l'ombre
-                              offset: Offset(0, 3), // Décalage de l'ombre
-                            ),
-                          ],
-                        ),
-                        width: double.infinity,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "${state.value.prix} FC",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 17.5,
-                                    fontWeight: FontWeight.bold),
+                            boxShadow: [
+                              BoxShadow(
+                                color:
+                                    // ignore: deprecated_member_use
+                                    Colors.black45, // Couleur de l'ombre
+                                spreadRadius: 2, // Rayon de propagation
+                                blurRadius: 5, // Flou de l'ombre
+                                offset: Offset(0, 3), // Décalage de l'ombre
                               ),
-                              PrimaryButton(
-                                status: (state.value.checkList.isEmpty) ? false : true,
-                                onPressed: () {},
-                                title: "Commander",
-                                long: false,
-                                fontSize: 16,
-                              )
                             ],
                           ),
-                        ),
-                      ))
-                ])
-              : Chargement(
-                  loading: state.value.loading,
-                  hasData: state.value.hasData,
-                ));
+                          width: double.infinity,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "${state.value.prix} FC",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 17.5,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                PrimaryButton(
+                                  status: (state.value.checkList.isEmpty)
+                                      ? false
+                                      : true,
+                                  onPressed: () {},
+                                  title: "Commander",
+                                  long: false,
+                                  fontSize: 16,
+                                )
+                              ],
+                            ),
+                          ),
+                        ))
+                  ])
+                : Chargement(
+                    loading: state.value.loading,
+                    hasData: state.value.hasData,
+                  ),
+            onRefresh: () async {
+              ctrl.getAll();
+            },
+            color: MyColors.primary,
+            backgroundColor: Colors.transparent,
+          ));
     });
   }
 }
